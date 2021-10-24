1d686741ed91e4c1c17c2f36b273f4235491f3c7
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-10-24 14:18:13 +0200

    create select location button

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -15,6 +15,7 @@ export default class TopSection extends React.Component {
         return <div className="top-container">
             <div className="title">Weather Up</div>
             <Weather />
+            <button className="btn btn-select-location">Select location</button>
         </div>
     }
 }
