5ebd636d06b1f31310ad5a1d5dd6c09b7981d82f
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-11-14 17:41:47 +0100

    add api

--- weather-app/weather-app/src/App.js
+++ weather-app/weather-app/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import "./sass/app.scss";
 
+const api = {
+  key: "c9aa4abf311c1251d6e33de88c50f5f1",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
