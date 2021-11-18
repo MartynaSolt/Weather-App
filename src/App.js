45f301f0472c1736fbe65508185babe28662fdfc
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-11-18 21:29:01 +0100

    add all scss styles

--- src/App.js
+++ src/App.js
@@ -36,7 +36,10 @@ function App() {
                     <div className="date">{dateBuilder(new Date())}</div>
                 </div>
                 <div className="weather-box">
-                    
+                    <div className="temp">
+                        15°C
+                    </div>
+                    <div className="weather">Sunny</div>
                 </div>
             </main> 
         </div>
